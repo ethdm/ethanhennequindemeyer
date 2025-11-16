89df14df645ac608f8a322855452ce2544e6da7b
ethdm <ethan.hennequin--de_meyer@edu.univ-fcomte.fr>
2025-11-16 23:06:05 +0100

    Add Mentions + Modif responsive

--- dist/server/entry.mjs
+++ dist/server/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_Dqya3tb8.mjs';
-import { manifest } from './manifest_By1uG7Uu.mjs';
+import { manifest } from './manifest_CmRlPZfm.mjs';
 
 const serverIslandMap = new Map();;
 
