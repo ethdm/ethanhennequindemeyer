8bd84f7134c6fbadeeac4edf9c00a3ae1101c543
ethdm <ethan.hennequin--de_meyer@edu.univ-fcomte.fr>
2025-11-18 14:42:14 +0100

    HFCFCCHC

--- dist/server/entry.mjs
+++ dist/server/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_Dqya3tb8.mjs';
-import { manifest } from './manifest_BJtHtCwy.mjs';
+import { manifest } from './manifest_3xtc6m2F.mjs';
 
 const serverIslandMap = new Map();;
 
