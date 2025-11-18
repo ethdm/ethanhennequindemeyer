5a691391f76fb319f31fd1e14d1d991989a979c3
ethdm <ethan.hennequin--de_meyer@edu.univ-fcomte.fr>
2025-11-18 21:16:15 +0100

    sdvsdv

--- dist/server/entry.mjs
+++ dist/server/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_Dqya3tb8.mjs';
-import { manifest } from './manifest_Da3DgZPv.mjs';
+import { manifest } from './manifest_C0l5e1mW.mjs';
 
 const serverIslandMap = new Map();;
 
