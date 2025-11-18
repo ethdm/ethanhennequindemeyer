78c589b80627d28b9b27a0dba1b82f26b7efcbbc
ethdm <ethan.hennequin--de_meyer@edu.univ-fcomte.fr>
2025-11-18 20:17:02 +0100

    ssvsv

--- src/lib/pb.ts
+++ src/lib/pb.ts
@@ -1,8 +1,8 @@
 import PocketBase from 'pocketbase';
-import type { TypedPocketBase } from "../lib/pocketbase-types";
+import type { TypedPocketBase } from "./pocketbase-types";
 var path = '';
 if (import.meta.env.MODE === 'development')
     path = 'http://localhost:8090/'    //localhost = machine de dev
-else path = 'https://portfolio.ethan-hennequindemeyer.fr/'   //localhost = machine de déploiement
+else path = 'https://portfolio.ethan-hennequindemeyer.fr/'   //url du site 
 const pb = new PocketBase(path) as TypedPocketBase;
 export default pb;
