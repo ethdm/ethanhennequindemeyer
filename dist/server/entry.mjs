aef83c8d57082018e2e14eadaf3d3d5b372dfea7
ethdm <ethan.hennequin--de_meyer@edu.univ-fcomte.fr>
2025-11-19 12:22:49 +0100

    dist

--- dist/server/entry.mjs
+++ dist/server/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_Dqya3tb8.mjs';
-import { manifest } from './manifest_BRjH-iVV.mjs';
+import { manifest } from './manifest_DeFfWGc3.mjs';
 
 const serverIslandMap = new Map();;
 
